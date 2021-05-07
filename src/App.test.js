07ffc6c15ef5e2a402f1ce839a3c1a309a5d2eb6
ayman <aymankaddioui@gmail.com>
2021-05-07 16:39:00 +0000

    change themplate

--- src/App.test.js
+++ src/App.test.js
@@ -1,21 +1,8 @@
-import React from 'react'
-import { shallow } from 'enzyme/build'
+import ReactDOM from 'react-dom'
 import App from './App'
-import ChartLineSimple from './views/charts/ChartLineSimple'
-import Dashboard from './views/dashboard/Dashboard.js'
 
-
-it('mounts App without crashing', () => {
-  const wrapper = shallow(<App/>)
-  wrapper.unmount()
-})
-
-it('mounts Dashboard without crashing', () => {
-  const wrapper = shallow(<Dashboard/>)
-  wrapper.unmount()
-})
-
-it('mounts Charts without crashing', () => {
-  const wrapper = shallow(<ChartLineSimple/> )
-  wrapper.unmount()
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+  ReactDOM.unmountComponentAtNode(div)
 })
--- src/views/Orders/MOrders/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function index() {
-  return (
-    <div>
-      <h1>Manage Orders :</h1>
-    </div>
-  );
-}
